b4334d132fc92aeef6ccfeae3c26171a26eb46e6
Joshua MARTINELLE <contact@jomar.fr>
2026-02-05 16:43:40 +0100

    MainLayout

---
+++ src/data/site.ts
@@ -0,0 +1,34 @@
+export const siteConfig = {
+  name: 'Joshua Martinelle',
+  shortName: 'Joshua Martinelle',
+  initials: 'JM',
+  role: 'Security Engineer @ Tenable',
+  secondaryRole: 'Bug Hunter',
+  description: 'Security Engineer & Bug Hunter - Joshua Martinelle',
+  bio: 'Security Researcher focusing on *Web Security*',
+};
+
+export const socialLinks = [
+  {
+    name: 'GitHub',
+    href: 'https://github.com/JoshuaMart',
+    icon: 'lucide:github',
+  },
+  {
+    name: 'X',
+    href: 'https://x.com/J0_mart',
+    icon: 'simple-icons:x',
+  },
+  {
+    name: 'LinkedIn',
+    href: 'https://www.linkedin.com/in/joshua-martinelle-a34911133/',
+    icon: 'lucide:linkedin',
+  },
+];
+
+export const navLinks = [
+  { href: '/', label: 'Home' },
+  { href: '/cves', label: 'CVEs' },
+  { href: '/blog', label: 'Blog' },
+  { href: '/tools', label: 'Tools' },
+];
