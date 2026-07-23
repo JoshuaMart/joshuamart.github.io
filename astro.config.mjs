84d4dfa3e8321f40a728d467b512749afd8a2d3d
Joshua MARTINELLE <contact@jomar.fr>
2026-07-23 08:50:23 +0200

    GEO improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import icon from 'astro-icon';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://blog.jomar.fr',
+  site: 'https://www.jomar.fr',
   vite: {
     plugins: [tailwindcss()]
   },
-  integrations: [icon()]
+  integrations: [icon(), sitemap()]
 });
